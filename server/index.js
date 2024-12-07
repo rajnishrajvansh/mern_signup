8f6eb74d563c78b73c7ce53f8cdefd8fdc90f6d6
rajnishrajvansh <kumar.rajnish6587@gmail.com>
2024-12-07 22:09:50 +0530

    Used bcrypt for hashing password

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const EmployeeModel = require("./model/Employee");
-const bcrypt=require('bcrypt')
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
@@ -14,26 +14,29 @@ app.post("/login", (req, res) => {
   const { email, password } = req.body;
   EmployeeModel.findOne({ email: email }).then((user) => {
     if (user) {
-      if (user.password === password) {
-        res.json("Success");
-      } else {
-        res.json("password is Incorrect");
-      }
+      bcrypt.compare(password, user.password, (err, response) => {
+        if (response) {
+          res.json("Success");
+        } else {
+          res.json("The password is incorrect");
+        }
+      });
     } else {
       res.json("No record exists");
     }
-  })
-})
+  });
+});
 
 app.post("/register", (req, res) => {
-    const {name,email,password}=(req.body)
-    bcrypt.hash(password,10)
-    .then(hash=>{
-        EmployeeModel.create({name,email,password: hash})
-    .then((employees) => res.json(employees))
-    .catch((err) => res.json);
+  const { name, email, password } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => {
+      EmployeeModel.create({ name, email, password: hash })
+        .then((employees) => res.json(employees))
+        .catch((err) => res.json);
     })
-    .catch(err=>console.log(err.message))
+    .catch((err) => console.log(err.message));
 });
 
 app.listen(3001, () => {
